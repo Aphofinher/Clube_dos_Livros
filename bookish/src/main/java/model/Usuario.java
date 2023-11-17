1a7aa8da8955101096bf106a3393f6029094b421
kelvin.loliveira <kelvin_oliveira@me.com>
2023-11-16 21:19:55 -0300

    Alteração da dao, e da tela de cadastrar

--- bookish/src/main/java/model/Usuario.java
+++ bookish/src/main/java/model/Usuario.java
@@ -5,7 +5,6 @@ public class Usuario {
     private String nome;
     private String email;
     private String senha;
-
     private String instagram;
 
     public Usuario(String email, String senha) {
